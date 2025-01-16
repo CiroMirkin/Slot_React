689dfecf6ad4045d7c75884f6d758d46b84a06b9
Ciro Mirkin <107360995+CiroMirkin@users.noreply.github.com>
2025-01-16 15:40:32 +0000

    third session

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,29 @@
 import './App.css'
 import { shapes } from './shapes'
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 
 function App() {
-  const [shape1, setShape1] = useState(0)
-  const [shape2, setShape2] = useState(1)
-  const [shape3, setShape3] = useState(2)
-
-  const [times, setTimes] = useState(-1)
+  const [shape1, setShape1] = useState([ 0, 1, 4, 3, 0 ])
+  const [shape2, setShape2] = useState([ 0, 1, 4, 3, 1 ])
+  const [shape3, setShape3] = useState([ 0, 1, 4, 3, 2 ])
 
   const setShapes = () => {
-    setShape1(Math.floor(Math.random() * shapes.length))
-    setShape2(Math.floor(Math.random() * shapes.length))
-    setShape3(Math.floor(Math.random() * shapes.length))
+    const shapes1 = Array(5).fill(0).map(s => Math.floor(Math.random() * shapes.length)) 
+    setShape1([...shapes1])
+    
+    const shapes2 = Array(5).fill(0).map(s => Math.floor(Math.random() * shapes.length)) 
+    setShape2([...shapes2])
+
+    const shapes3 = Array(5).fill(0).map(s => Math.floor(Math.random() * shapes.length)) 
+    setShape3([...shapes3])
   }
 
-  const getResultOfUserPlay = () => {
-    if(shape1 === shape2 && shape2 === shape3) {
-      // The three shapes are the same
+  const getResultOfUserPlay = (s1, s2, s3) => {
+    if(s1 === s2 && s2 === s3) {
+      // The three ss are the same
       return ('You win')
     }
-    else if (shape1 === shape2 || shape2 === shape3 || shape1 === shape3 || shape1 === shape2) {
+    else if (s1 === s2 || s2 === s3 || s1 === s3 || s1 === s2) {
       // Two shapes are the same
       return (50)
     }
@@ -29,29 +32,30 @@ function App() {
     }
   }
 
+  const [shapesIndex, setShapesIndex] = useState(0)
+
   const handleClick = () => {
-    setTimes(5)
+    setShapesIndex(0)
+    setShapes()
 
     console.log('click')
   }
 
-  if(times > 0) {
-    console.log('decremento ', times)
 
-    const newTime = times - 1
-    setTimes(newTime)
-
-    setTimeout(setShapes, 1000)
-  } 
-  if(times === 0) getResultOfUserPlay()
+  setInterval(() => {
+    if(shapesIndex <= 5) {
+      const newShapesIndex = shapesIndex + 1
+      setShapesIndex(newShapesIndex)
+    }
+  }, 1000)
 
   return (
     <>
-      <h2>{}</h2>
+      <h2>{ getResultOfUserPlay(shape1.at(-1), shape2.at(-1), shape3.at(-1)) }</h2>
       <div className="slot">
-        <span>{shapes[shape1]}</span>
-        <span>{shapes[shape2]}</span>
-        <span>{shapes[shape3]}</span>
+        <span>{ shapes[shape1[shapesIndex]] }</span>
+        <span>{ shapes[shape2[shapesIndex]] }</span>
+        <span>{ shapes[shape3[shapesIndex]] }</span>
       </div>
       <button onClick={handleClick}>play</button>
     </>
