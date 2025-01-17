38aa28da7dab344d89fa74de52ab16fde9417c7f
Ciro Mirkin <107360995+CiroMirkin@users.noreply.github.com>
2025-01-17 14:26:56 +0000

    sixth session

--- src/App.jsx
+++ src/App.jsx
@@ -1,70 +1,10 @@
 import './App.css'
-import { shapes } from './shapes'
-import { useState } from 'react'
+import Slot from './Slot'
 
 function App() {
-  const [shape1, setShape1] = useState(0)
-  const [shape2, setShape2] = useState(1)
-  const [shape3, setShape3] = useState(2)
-
-  const [count, setCount] = useState(0)
-  const [state, setState] = useState(false)
-
-  const [play, setPlay] = useState("")
-
-  const amountOfSpins = 10
-
-  const setShapes = () => {
-    console.log("change ", count)
-
-    setCount(count + 1)
-
-    setShape1(Math.floor(Math.random() * shapes.length))
-    setShape2(Math.floor(Math.random() * shapes.length))
-    setShape3(Math.floor(Math.random() * shapes.length))
-
-  }
-
-  const getResultOfUserPlay = (s1, s2, s3) => {
-    if (s1 === s2 && s2 === s3) {
-      // The three ss are the same
-      return ('You win')
-    }
-    else if (s1 === s2 || s2 === s3 || s1 === s3 || s1 === s2) {
-      // Two shapes are the same
-      return (50)
-    }
-    else {
-      return ('you lost')
-    }
-  }
-
-  const handleClick = () => {
-    setState(true)
-    console.log('click')
-  }
-
-  if (count === amountOfSpins) {
-    setPlay(getResultOfUserPlay(shape1, shape2, shape3))
-    setState(false)
-    setCount(0)
-  }
-  
-  setTimeout(() => {
-    if (state && count < amountOfSpins) {
-      setShapes()
-    }
-  }, 500)
-
   return (
     <>
-      <h2>{play}</h2>
-      <div className="slot">
-        <span>{shapes[shape1]}</span>
-        <span>{shapes[shape2]}</span>
-        <span>{shapes[shape3]}</span>
-      </div>
-      <button onClick={handleClick}>play</button>
+      <Slot />
     </>
   )
 }
---
+++ src/Slot.jsx
@@ -0,0 +1,75 @@
+import './Slot.css'
+import { shapes } from './shapes'
+import { useState, useEffect } from 'react'
+
+const getResultOfUserPlay = (s1, s2, s3) => {
+    if (s1 === s2 && s2 === s3) {
+        // The three shapes are the same
+        return ('You win')
+    }
+    else if (s1 === s2 || s2 === s3 || s1 === s3 || s1 === s2) {
+        // Two shapes are the same
+        return ('50')
+    }
+    else {
+        return ('you lost')
+    }
+}
+
+function Slot({ }) {
+    const [shapesIndex, setShapesIndex] = useState([0, 1, 2])
+    const [countOfSpins, setCountOfSpins] = useState(0)
+    const [theSlotIsOn, setTheSlotIsOn] = useState(false)
+
+    const playResult = theSlotIsOn ? '???' : getResultOfUserPlay(...shapesIndex)
+    const amountOfSpins = 11
+
+    const setShapes = () => {
+        setCountOfSpins(countOfSpins + 1)
+
+        const newShapesIndex = [
+            Math.floor(Math.random() * shapes.length),
+            Math.floor(Math.random() * shapes.length),
+            Math.floor(Math.random() * shapes.length),
+        ]
+        setShapesIndex([...newShapesIndex])
+    }
+
+    // If the slot made all the spins it has to make
+    if (countOfSpins === amountOfSpins) {
+        setTheSlotIsOn(false)
+        setCountOfSpins(0)
+    }
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            // If the slot is spinning
+            if (theSlotIsOn && countOfSpins < amountOfSpins) setShapes()
+        }, 300)
+        return () => clearTimeout(timer)
+    }, [countOfSpins, theSlotIsOn])
+
+    const handleClick = () => setTheSlotIsOn(true)
+
+    return (
+        <>
+            <h2>{playResult}</h2>
+
+            <div className="slot">
+                <span className='slot__shape'>
+                    {shapes[shapesIndex[0]]}
+                </span>
+                <span className='slot__shape'>
+                    {shapes[shapesIndex[1]]}
+                </span>
+                <span className='slot__shape'>
+                    {shapes[shapesIndex[2]]}
+                </span>
+            </div>
+
+            <button onClick={handleClick}>play</button>
+        </>
+    )
+}
+
+export default Slot
